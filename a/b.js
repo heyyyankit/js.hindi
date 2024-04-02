63f2ce19a274a62b693060bcb5f519404aad9f11
heyyyankit <155145624+heyyyankit@users.noreply.github.com>
2024-04-02 08:47:43 +0000

    commit oper

--- a/b.js
+++ a/b.js
@@ -9,3 +9,16 @@ let ss = "23"
 let sa = Boolean(s)
 let ssa = Boolean(ss)
 console.log([s,ss,sa,ssa])
+// push again
+console.log("2"+2+2)
+console.log(2+2+"2")
+console.log(+true)
+console.log(+"")
+// console.log(++"5")
+// console.log(++5)
+// console.log(5++)
+let aa = 5;
+console.log(aa)
+console.log(++aa)
+aa++
+console.log(aa)
