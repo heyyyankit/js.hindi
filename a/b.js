68868a0666e036156c12ba04dfefd353b61369ad
heyyyankit <155145624+heyyyankit@users.noreply.github.com>
2024-04-02 08:33:28 +0000

    conversion push

---
+++ a/b.js
@@ -0,0 +1,11 @@
+"use strict";
+let a = "35a"
+console.log(typeof a)
+let b = Number(a)
+console.log(typeof b)
+console.log(b)
+let s = "1"
+let ss = "23"
+let sa = Boolean(s)
+let ssa = Boolean(ss)
+console.log([s,ss,sa,ssa])
