01f403433ffda49a76a69979072d7ac81bb5025e
heyyyankit <155145624+heyyyankit@users.noreply.github.com>
2024-04-03 18:07:52 +0000

    refernce and copy commit

--- basics2/d.js
+++ basics2/d.js
@@ -7,3 +7,24 @@ console.log(myobj.score)
 const myfunc = function(){
     console.log(x)
 }
+
+///////////////////////////////////////////////////////
+// array, object and function in heap (reference)
+// other variales in stack (int,string)
+
+
+
+let myobj2 = myobj;
+console.log(myobj.name)
+
+myobj2.name = "kumar";
+console.log(myobj.name)
+
+// here changed at both places (as reference)
+
+let string1 = "abcde"
+let string2 = string1
+console.log(string1)
+
+string2 = "fghij"   // here changed in only string2, not in string1, as copy is made
+console.log(string1)
